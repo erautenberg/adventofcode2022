f275d4fcb7ed04f5dda95451011be44206659c1b
Rautenberg <emily_rautenberg@comcast.com>
2022-12-14 16:09:01 -0500

    feat: Project Setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# adventofcode2022
+My solutions to Advent of Code 2022 (https://adventofcode.com/2022)
+
+## To Run:
+- `npm install`
+- `npm start`
+- Navigate to http://localhost:2022/
---
+++ public/src/1.js
@@ -0,0 +1,7 @@
+const DAY1 = 1;
+parseData(DAY1, (input) => {
+  const expenses = input.map(n => parseInt(n));
+  const part1 = null;
+  const part2 = null;
+  showAnswers(DAY1, part1, part2);
+});

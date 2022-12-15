a5da36a1d902d3a5939dbeef2b7349c149ee63a2
Rautenberg <emily_rautenberg@comcast.com>
2022-12-15 11:49:53 -0500

    feat(Day3): solution for part 1

---
+++ public/src/3.js
@@ -0,0 +1,75 @@
+const DAY3 = 3;
+parseData(DAY3, (input) => {
+  const part1 = runPart1(input);
+  const part2 = runPart2(input);;
+  showAnswers(DAY3, part1, part2);
+});
+
+/**
+ * Calculates the answer to Day 3, Part 1
+ * 
+ * @param {array} input 
+ * @returns {number}
+ */
+function runPart1(input) {
+  const rucksacks = parseRucksacks(input);
+  const priorities = rucksacks.map(rucksack => getItemPriority(findItemInBothCompartments(rucksack)));
+  return priorities.reduce((acc, curr) => acc + curr, 0);
+}
+
+/**
+ * Calculates the answer to Day 3, Part 2
+ * 
+ * @param {array} input 
+ * @returns {number}
+ */
+function runPart2(input) {
+  return;
+}
+
+/**
+ * Returns an array of 2D arrays where each element is an array that represents a rucksack,
+ * and each element contains an array of length two, denoting the 2 compartments of the rucksack.
+ * Each compartment array then represents an item in the rucksack.
+ * 
+ * @param {array} rucksacks 
+ * @returns {array}
+ */
+function parseRucksacks(rucksacks) {
+  return rucksacks.map(rucksack => {
+    const items = rucksack.split("");
+    const middleIndex = Math.ceil(items.length / 2);
+    return [items.slice(0, middleIndex), items.slice(middleIndex, items.length)];
+  });
+}
+
+/**
+ * Finds the item that exists in both compartments of the rucksack.
+ * 
+ * @param {array} rucksack 
+ * @returns {string}
+ */
+function findItemInBothCompartments(rucksack) {
+  return rucksack[0].filter(item => rucksack[1].includes(item))[0];
+}
+
+/**
+ * Get the "priority" number of an item, where:
+ * Lowercase item types a through z have priorities 1 through 26 and
+ * Uppercase item types A through Z have priorities 27 through 52.
+ * 
+ * @param {string} item 
+ * @returns {number}
+ */
+function getItemPriority(item) {
+  // Uppercase A is 65 on the ascii table, but it is 0-based,
+  // so we need to offset the character code by 64 (65 - 1),
+  // and the uppercase items are priority 27-52,
+  // so an additional offset is needed as they are the second half of the priority list (64 - 26 = 38).
+  // Lowercase a is 97 on the ascii table, but it is 0-based,
+  // so we need to offset the character code by 96 (97 - 1),
+  // and the lowercase items are priority 1-26,
+  // so no other offset is needed as they are the beginning of the priority list (96 - 0 = 0).
+  const offset = item === item.toUpperCase() ? 38 : 96;
+  return item.charCodeAt(0) - offset;
+}
